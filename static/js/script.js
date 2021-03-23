94e4f13062572634dc9a700ee100f184e4e1a738
LiamGaff <liamgaffneytya@gmail.com>
2021-03-23 10:41:37 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
   $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
-    $('.tooltipped').tooltip()
+    $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        il8n: {
+            done: "select"
+        }
+    });
   });
 
